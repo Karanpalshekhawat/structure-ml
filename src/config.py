3f7cbd5cff553f32e0046f979ff3338867fb8d30
karanpalsinghshekhawat <karanpal609@gmail.com>
2020-10-25 16:38:35 +0530

    automatic folds creation in the dataframe for cross validation

--- src/config.py
+++ src/config.py
@@ -2,3 +2,6 @@
 This script is used to initialise the important
 static variables
 """
+
+TRAINING_FILE = r'//Users/karanpalsinghshekhawat/Desktop/archive/mnist_train.csv'
+TEST_FILE = r'/Users/karanpalsinghshekhawat/Desktop/archive/mnist_test.csv'
